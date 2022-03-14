0b25177a98a74242608f4e4f6e44c1316f938790
sindurasaraswathi <sindurasaraswathi@gmail.com>
2022-03-13 21:07:05 -0500

    Integrated html pages

--- AgriHelp/app.py
+++ AgriHelp/app.py
@@ -1,20 +1,46 @@
-# import the Flask class from the flask module
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
 
-# create the application object
+This is a Python script file.
+Author : Sindura Saraswathi
+Date: 3/7/2022
+"""
+
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
-
-# use decorators to link the function to a url
 @app.route('/')
 def home_page():
     return render_template('base.html')
 
-@app.route('/login', methods=['POST'])
+
+@app.route('/login')
 def login_page():
     return render_template('login.html')
 
 
-# start the server with the 'run()' method
-if __name__ == '__main__':
+@app.route('/login/', methods=["POST"])
+def login_check():
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['pwd']
+        return '<p>User logged in</p>'
+
+
+@app.route('/signup')
+def sign_up():
+    return render_template('SignUp.html')
+
+@app.route('/createaccount', methods=["POST"])
+def create_account():
+    if request.method == 'POST':
+        username = request.form['Username']
+        email = request.form['email']
+        password = request.form['password']
+        return '<p>User account created and logged in</p>'
+
+
+if '__main__' == __name__:
     app.run()
